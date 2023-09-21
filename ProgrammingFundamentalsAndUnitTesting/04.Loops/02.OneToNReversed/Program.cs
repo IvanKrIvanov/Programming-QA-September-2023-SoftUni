7e18dd77f1700392588839fc2fc0c0eb2b4f448f
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-09-21 17:05:46 +0300

    Loops: While and For Loops. Nested Loops

---
+++ ProgrammingFundamentalsAndUnitTesting/04.Loops/02.OneToNReversed/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.OneToNReversed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i += 1)
+            {
+                Console.WriteLine(i);
+            }
+                    }
+    }
+}
