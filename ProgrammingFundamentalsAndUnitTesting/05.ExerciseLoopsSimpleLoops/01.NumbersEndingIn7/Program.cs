5d2d6c2b5511d82ebafd59977004338522a17789
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-09-25 17:54:50 +0300

    Exercises: Simple Loops

---
+++ ProgrammingFundamentalsAndUnitTesting/05.ExerciseLoopsSimpleLoops/01.NumbersEndingIn7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.NumbersEndingIn7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 997; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //for (int i = 7; i <= 997; i++)
+            //{
+            //    if (i % 10 == 7)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+        }
+    }
+}
