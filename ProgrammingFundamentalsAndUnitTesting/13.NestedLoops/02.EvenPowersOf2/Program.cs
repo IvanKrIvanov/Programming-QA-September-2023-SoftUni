1b97cabc04f778c811f2cffc9d518d764a304801
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-09 21:28:35 +0300

    Nested Loops

---
+++ ProgrammingFundamentalsAndUnitTesting/13.NestedLoops/02.EvenPowersOf2/Program.cs
@@ -0,0 +1,7 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= n; i += 2)
+{
+    double powerOf2 = Math.Pow(2, i);
+    Console.WriteLine(powerOf2);
+}
---
+++ ProgrammingFundamentalsAndUnitTesting/13.NestedLoops/03.TriangleОfStars/Program.cs
@@ -0,0 +1,10 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= size; row++)
+{
+	for (int numberOfStar = 1; numberOfStar <= row; numberOfStar++)
+	{
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
