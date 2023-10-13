0495c1077e9dfd428a2440bbd765867c74c0afec
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-13 20:19:58 +0300

    Exercise: Nested Loops and Methods

--- ProgrammingFundamentalsAndUnitTesting/15.ExerciseNestedLoopsАndMethods/07.FactorialDivision/Program.cs
+++ ProgrammingFundamentalsAndUnitTesting/15.ExerciseNestedLoopsАndMethods/07.FactorialDivision/Program.cs
@@ -1,6 +1,12 @@
 ﻿int number1 = int.Parse(Console.ReadLine());
 int number2 = int.Parse(Console.ReadLine());
 
+int factorial1 = CalculateFactorial(number1);
+int factorial2 = CalculateFactorial(number2);
+
+double result = (double)factorial1 / factorial2;
+Console.WriteLine(result);
+
 static int CalculateFactorial(int n)
 {
     if (n == 0 || n == 1)
@@ -10,10 +16,3 @@ static int CalculateFactorial(int n)
 
     return n * CalculateFactorial(n - 1);
 }
-
-int factorial1 = CalculateFactorial(number1);
-int factorial2 = CalculateFactorial(number2);
-
-double result = (double)factorial1 / factorial2;
-Console.WriteLine(result);
-
