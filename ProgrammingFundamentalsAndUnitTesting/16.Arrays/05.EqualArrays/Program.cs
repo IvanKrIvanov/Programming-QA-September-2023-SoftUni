4944d0d49c757720c6d42652a2d376fb706c80fc
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-17 20:55:12 +0300

    16. Arrays

---
+++ ProgrammingFundamentalsAndUnitTesting/16.Arrays/05.EqualArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] firstArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secondArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+bool isIdentical = true;
+
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+	if (firstArray[i] != secondArray[i])
+	{
+        isIdentical = false;
+        Console.WriteLine("Arrays are not identical.");
+        break;
+    }
+}
+
+if (isIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
---
+++ ProgrammingFundamentalsAndUnitTesting/16.Arrays/06.CommonElements/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] firstArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secondArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+var commonElements = firstArray.Intersect(secondArray);
+
+foreach (int element in commonElements)
+{
+    Console.Write(element + " ");
+}
+
+Console.WriteLine();
