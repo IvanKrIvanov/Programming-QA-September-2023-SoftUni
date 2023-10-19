11b9cabd9c43658c4d1222162d741feeb079374b
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-19 17:54:39 +0300

    16.Arrays/06.CommonElements

--- ProgrammingFundamentalsAndUnitTesting/16.Arrays/06.CommonElements/Program.cs
+++ ProgrammingFundamentalsAndUnitTesting/16.Arrays/06.CommonElements/Program.cs
@@ -1,11 +1,18 @@
 ﻿int[] firstArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
 int[] secondArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
 
-var commonElements = firstArray.Intersect(secondArray);
 
-foreach (int element in commonElements)
+
+foreach (int number in firstArray)
 {
-    Console.Write(element + " ");
+	foreach (int number2 in secondArray)
+	{
+		if (number == number2)
+		{
+            Console.WriteLine(number + " ");
+			break;
+        }
+	}
 }
 
-Console.WriteLine();
+
