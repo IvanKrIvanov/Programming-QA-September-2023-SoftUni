29e9f7820afa6044aaadb7713e6c8c8625901d9e
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-20 07:10:54 +0300

    18.ExerciseArraysАndLists

---
+++ ProgrammingFundamentalsAndUnitTesting/18.ExerciseArraysAndLists/08.ArrayRotation/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    int cirrentNum = input[0];
+    input.RemoveAt(0);
+    input.Add(cirrentNum);
+}
+Console.WriteLine(string.Join(" ", input));
