bfb475734f853c02e616b521dd60705ac2963199
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-24 20:54:37 +0300

    Unit Testing - Methods

---
+++ ProgrammingFundamentalsAndUnitTesting/19.UnitTestMethods/TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = "";
+
+        // Act
+        string actual = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("", actual);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        // Arrange
+        string input = "X";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("X", result);
+        Assert.AreEqual(1, result.Length);
+
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        // Arrange
+        string input = "XYZ";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(input.Length, input.Length);
+        Assert.AreEqual("ZYX", result);
+
+    }
+}
