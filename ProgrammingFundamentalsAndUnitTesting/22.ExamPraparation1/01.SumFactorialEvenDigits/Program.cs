8a0c8689e703cb4aca5e6fa0ed2aea486575dbd6
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-10-30 20:58:22 +0200

    Exam Preparation 1

---
+++ ProgrammingFundamentalsAndUnitTesting/22.ExamPraparation1/01.SumFactorialEvenDigits/Program.cs
@@ -0,0 +1,32 @@
+﻿int result = int.Parse(Console.ReadLine());
+
+int totalResult = 0;
+
+while (result != 0)
+{
+    int digit = result % 10;
+
+    if (digit % 2 == 0)
+    {
+        int resultFactorial = CalsulateFactorial(digit);
+        totalResult += resultFactorial;
+    }
+
+    result = result / 10;
+}
+
+Console.WriteLine(totalResult);
+
+static int CalsulateFactorial(int number)
+{
+    int result = 1;
+
+    while (number > 0)
+    {
+        result = result * number;
+        number -= 1;
+    }
+
+
+    return result;
+}
---
+++ ProgrammingFundamentalsAndUnitTesting/22.ExamPraparation1/02.MiddleElements/Program.cs
@@ -0,0 +1,8 @@
+﻿double[] inputArray = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+
+int middleRightElementIndex = inputArray.Length / 2;
+int middleLeftElementIndex = middleRightElementIndex - 1;
+
+double middleElementsAverage = (inputArray[middleRightElementIndex] + inputArray[middleLeftElementIndex]) / 2;
+
+Console.WriteLine($"{middleElementsAverage:F2}");
