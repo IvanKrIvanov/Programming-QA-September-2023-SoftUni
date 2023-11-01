053a5a8db9f06210c9082c07a05770a9a3a36da4
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-11-01 11:02:50 +0200

    Exam Preparation 2

---
+++ ProgrammingFundamentalsAndUnitTesting/23.ExamPreparation2/01.MagicNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Linq;
+
+int n = int.Parse(Console.ReadLine());
+
+bool isThereNoMatchNumbers = true;
+
+for (int i = 1; i <= n; i++)
+{
+    int workNum = i;
+    int digitSum = 0;
+    bool isAllDigitPrime = true;
+
+    while (workNum > 0)
+    {
+        int digit = workNum % 10;
+        workNum = workNum / 10;
+
+        bool isDigitPrime = digit == 2 || digit == 3 || digit == 5 || digit == 7;
+
+        if (isDigitPrime)
+        {
+            digitSum += digit;
+        }
+        else
+        {
+            isAllDigitPrime = false;
+            break;
+        }
+    }
+    if (isAllDigitPrime && digitSum % 2 == 0)
+    {
+        Console.Write(i + " ");
+        isThereNoMatchNumbers = false;
+    }
+}
+if (isThereNoMatchNumbers)
+{
+    Console.WriteLine("no");
+}
---
+++ ProgrammingFundamentalsAndUnitTesting/23.ExamPreparation2/02.MinMaxValues/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int selection = int.Parse(Console.ReadLine());
+
+int max = int.MinValue;
+int min = int.MaxValue;
+
+for (int i = 0; i < selection; i++)
+{
+	int currentInt = ints[i];
+	if (max < currentInt)
+	{
+		max = currentInt;
+	}
+    if (min > currentInt)
+    {
+        min = currentInt;
+    }
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
