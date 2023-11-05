b85b1f1d0c78b9d1a5d507a651016747bf3105a8
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-11-05 10:27:33 +0200

    Exam

---
+++ ProgrammingFundamentalsAndUnitTesting/Exam/01.CalculateAverageGrade/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+double sumOfGrades = 0.0;
+int numberOfStudents = 0;
+
+for (int i = 0; i < n; i++)
+{
+    double grade = double.Parse(Console.ReadLine());
+    sumOfGrades += grade;
+    numberOfStudents++;
+}
+double averageGrade = sumOfGrades / numberOfStudents;
+
+
+Console.WriteLine($"{averageGrade:F2}");
