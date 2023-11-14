ae8610f62202a8f5f76b6b8d1ee73a7233b854b3
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-11-14 21:06:25 +0200

    Exercises: Dictionaries, Lambda and LINQ

---
+++ ProgrammingAdvancedForQA/05.ExercisesDictionariesLambdaLINQ/04.SoftUniParking/Program.cs
@@ -0,0 +1,44 @@
+﻿Dictionary<string, string> registrars = new();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] commandArray = Console.ReadLine().Split().ToArray();
+
+    string command = commandArray[0];
+    string employee = commandArray[1];
+
+    if (command == "register")
+    {
+
+        string plateName = commandArray[2];
+
+        if (!registrars.ContainsKey(employee))
+        {
+            registrars.Add(employee, plateName);
+            Console.WriteLine($"{employee} registered {plateName} successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: already registered with plate number {registrars[employee]}");
+        }
+    }
+    else if (command == "unregister")
+    {
+        if (registrars.ContainsKey(employee))
+        {
+            registrars.Remove(employee);
+            Console.WriteLine($"{employee} unregistered successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: user {employee} not found");
+        }
+    }
+}
+
+foreach (var pair in registrars)
+{
+    Console.WriteLine($"{pair.Key} => {pair.Value}");
+}
