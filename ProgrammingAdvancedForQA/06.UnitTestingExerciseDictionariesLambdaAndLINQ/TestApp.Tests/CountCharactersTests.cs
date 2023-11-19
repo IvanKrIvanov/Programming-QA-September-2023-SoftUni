1ccd1babd17727559f8c8da0cb7505279654082d
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-11-19 15:44:19 +0200

    Unit Testing Exercise Dictionaries, Lambda and LINQ

---
+++ ProgrammingAdvancedForQA/06.UnitTestingExerciseDictionariesLambdaAndLINQ/TestApp.Tests/CountCharactersTests.cs
@@ -0,0 +1,113 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.Tests;
+
+public class CountCharactersTests
+{
+    [Test]
+    public void Test_Count_WithEmptyList_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithNoCharacters_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new() { "", "", "" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleCharacter_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() {  "a" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("a -> 1"));
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "aaa", "aabbccc", "abcbba" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("a -> 7");
+        sb.AppendLine("b -> 5");
+        sb.AppendLine("c -> 4");
+        string expexted = sb.ToString().Trim();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expexted));
+    }
+
+    [Test]
+    public void Test_Count_WithSpecialCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "aa!a", "!aabbc!cc", "abc!bcba" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("a -> 7");
+        sb.AppendLine("! -> 4");
+        sb.AppendLine("b -> 5");
+        sb.AppendLine("c -> 5");
+        string expexted = sb.ToString().Trim();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expexted));
+    }
+    [Test]
+    public void Test_Count_WithUpperCaseAndNumberCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "aa!a", "!aabbc!cc", "abc!bcba", "ABC123", "CBA123", "BCA123" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("a -> 7");
+        sb.AppendLine("! -> 4");
+        sb.AppendLine("b -> 5");
+        sb.AppendLine("c -> 5");
+        sb.AppendLine("A -> 3");
+        sb.AppendLine("B -> 3");
+        sb.AppendLine("C -> 3");
+        sb.AppendLine("1 -> 3");
+        sb.AppendLine("2 -> 3");
+        sb.AppendLine("3 -> 3");
+        string expexted = sb.ToString().Trim();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expexted));
+    }
+}
---
+++ ProgrammingAdvancedForQA/06.UnitTestingExerciseDictionariesLambdaAndLINQ/TestApp.Tests/OrdersTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class OrdersTests
+{
+    [Test]
+    public void Test_Order_WithEmptyInput_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] input = new string[0];
+
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Order_WithMultipleOrders_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 1.99 1",
+            "banana 0.75 3",
+            "banana 0.75 2",
+            "orange 0.99 2",
+            "apple 1.99 2",
+        };
+
+       // Act
+       string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"apple -> 5.97{Environment.NewLine}banana -> 3.75{Environment.NewLine}orange -> 1.98"));
+    }
+
+    [Test]
+    public void Test_Order_WithRoundedPrices_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 1.99 1",
+            "banana 0.750000000000005 3",
+            "banana 0.7500000000000009 2",
+            "orange 0.990000000000000001 2",
+            "apple 1.99 2",
+        };
+
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"apple -> 5.97{Environment.NewLine}banana -> 3.75{Environment.NewLine}orange -> 1.98"));
+    }
+
+    [Test]
+    public void Test_Order_WithDecimalQuantities_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 2.00 4.5",
+            "banana 3.00 2.5",
+            "orange 1.00 3.5",
+        };
+
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"apple -> 9.00{Environment.NewLine}banana -> 7.50{Environment.NewLine}orange -> 3.50"));
+    }
+}
