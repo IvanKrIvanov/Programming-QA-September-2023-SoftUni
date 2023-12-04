455b6d4ffda7e0071a19842ef0918d609a71f9e0
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-12-04 21:18:13 +0200

    OOP Principles: Encapsulation and Inheritance

---
+++ ProgrammingAdvancedForQA/13.OOPPrinciplesEncapsulationAndInheritance/Players/BladeKnight.cs
@@ -0,0 +1,8 @@
+﻿namespace Players;
+
+public class BladeKnight : DarkKnight
+{
+    public BladeKnight(string userName, int level) : base(userName, level)
+    {
+    }
+}
---
+++ ProgrammingAdvancedForQA/13.OOPPrinciplesEncapsulationAndInheritance/Players/DarkKnight.cs
@@ -0,0 +1,8 @@
+﻿namespace Players;
+
+public class DarkKnight : Knight
+{
+    public DarkKnight(string userName, int level) : base(userName, level)
+    {
+    }
+}
