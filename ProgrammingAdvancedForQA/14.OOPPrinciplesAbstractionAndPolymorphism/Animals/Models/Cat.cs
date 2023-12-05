47cba9227e65852c55ceec8b43a1f10a852e07e2
Ivan Ivanov <ivanov.ivan.kr@gmail.com>
2023-12-05 22:28:00 +0200

    OOP Principles: Abstraction and Polymorphism

---
+++ ProgrammingAdvancedForQA/14.OOPPrinciplesAbstractionAndPolymorphism/Animals/Models/Cat.cs
@@ -0,0 +1,14 @@
+﻿using Animals.Models;
+
+internal class Cat : Animal
+{
+    public Cat(string name, string favoriteFood) : base(name, favoriteFood)
+    {
+
+    }
+
+    public override string ExplainSelf()
+    {
+        return base.ExplainSelf() + "\nMEEOW";
+    }
+}
---
+++ ProgrammingAdvancedForQA/14.OOPPrinciplesAbstractionAndPolymorphism/Raiding/Models/Fighter.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Raiding.Models;
+
+public abstract class Fighter : BaseHero
+{
+    public Fighter(string name) : base(name) { }
+
+    public override string ToString()
+    {
+        return base.CastAbility() + $"hit for {this.Power} damage";
+    }
+}
